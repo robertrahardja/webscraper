4924af7ef5a140162b2d7ff284cd6672257606a6
Robert Rahardja <robertrahardja@Roberts-MacBook-Pro.local>
2020-10-29 16:47:19 +0800

    added second array in main.js

--- main.js
+++ main.js
@@ -6,12 +6,13 @@ const url = 'https://wallpapers.com/sitemap.xml'
 ;(async () => {
   let Arr = await webscraper(url)
   //   console.info(Arr)
-
+  let Arr2 = await webscraper(url)
+  
   //compare arrays
   var arrayLength = Arr.length
 //   for (var i = 0; i < arrayLength; i++) {
     for (var i = 0; i < 50; i++) {//for debugging
-    let output = await compareUrls(Arr[i], Arr[i])
+    let output = await compareUrls(Arr[i], Arr2[i])
     console.log(output)
   }
 })()
